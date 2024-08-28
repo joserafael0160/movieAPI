a598a1817742c7b4b60d240c341dc73bf75f64cf
joserafael0160 <joserafaelperezrivero113@gmail.com>
2024-08-27 22:20:06 -0400

    fix cors problem

--- app.js
+++ app.js
@@ -1,11 +1,31 @@
 const express = require("express");
 const crypto = require("node:crypto");
 const movies = require("./movies.json");
+const cors = require("cors");
 const { validateMovie, validatePartialMovie } = require("./schemas/movies");
+const { error } = require("node:console");
 const app = express();
 app.disable("x-powered-by");
 
 app.use(express.json());
+app.use(cors({
+  origin: (origin, callback) => {
+    const ACCEPTED_ORIGINS = [
+      "http://localhost:8080",
+      "http://localhost:1234"
+    ];
+
+    if (ACCEPTED_ORIGINS.includes(origin)) {
+      return callback(null, true);
+    }
+
+    if (!origin) {
+      return callback(null, true);
+    }
+
+    return callback(new Error("Not allowed by CORS"));
+  }
+}));
 
 app.get("/", (req, res) => {
   res.json({ message: "hola mundo" });
@@ -47,6 +67,19 @@ app.post("/movies", (req, res) => {
   res.status(201).json(newMovie);
 });
 
+app.delete("/movies/:id", (req, res) => {
+  const { id } = req.params;
+  const movieIndex = movies.findIndex(movie => movie.id === id);
+
+  if (movieIndex === -1) {
+    return res.status(404).json({ message: "Movie not found" });
+  }
+
+  movies.splice(movieIndex, 1);
+
+  return res.json({ message: "Movie deleted" });
+});
+
 app.patch("/movies/:id", (req, res) => {
   const result = validatePartialMovie(req.body);
 
@@ -67,7 +100,15 @@ app.patch("/movies/:id", (req, res) => {
   movies[movieIndex] = updateMovie;
   return res.json(updateMovie);
 });
+app.options("/movies/:id", (req, res) => {
+  const origin = req.header("origin");
 
+  if (ACCEPTED_ORIGINS.includes(origin) || !origin) {
+    res.header("Access-Control-Allow-Origin", origin);
+    res.header("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
+  }
+  res.send(200);
+});
 const PORT = process.env.PORT ?? 1234;
 
 app.listen(PORT, () => {
